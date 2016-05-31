d7d479ec02e5e1db8e0763418da35d5949584628
konata39 <akiyamakonata@gmail.com>
2016-06-01 01:36:01 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,10 @@
-# -*- coding: BIG5 -*-
-# coding: BIG5
+# -*- coding: utf8 -*-
+# coding: utf8
 # 這邊宣告
 import sys
 import re
 reload(sys)
-sys.setdefaultencoding('BIG5')
+sys.setdefaultencoding('utf8')
 
 #這邊切command line字串
 #先看是不是3個argv 不是就fail
@@ -21,12 +21,6 @@ print "\n".join(spilt_pair)
 #用#分隔馬英九#蔡因文
 pair_1 = spilt_pair[1].split("#")
 
-#變成regular expression grammer
-regular_1 = r".*("+re.escape(pair_1[0])+r"|"+re.escape(pair_1[1])+r").*("+re.escape(pair_1[0])+r"|"+re.escape(pair_1[1])+r").*\n"
-
-#看有沒有錯
-print regular_1
-
 #讀黨
 file = open(sys.argv[2], 'r')
 
@@ -37,11 +31,8 @@ content = file.read()
 content_str = str(content).split("\n")
 
 #檢查符不符合regular exprewwion
-m =  re.search(regular_1, content_str[417])
+m = re.search(pair_1[0],content_str[417])
 #m =  re_words.search(content_str,0)
-
-#印出regulare expression
-print regular_1
 
 #印出有馬英九跟蔡英文對的該筆資料
 print content_str[417]
